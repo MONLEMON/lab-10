83c2dc5767b431152fbb2efc6e1ad566c78afc58
Surabordin <bluelimec@gmail.com>
2021-12-14 17:03:07 +0700

    lab 10 -2 complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int printO(int x,int y){
+	if (x <= 0 or y <= 0){
+		cout << "Invalid input";
+	}else{
+	for(int i=0; i<x;i++){
+		for(int j=0; j<y;j++){
+			cout << 'O';
+		}
+		cout << '\n';
+	}
+	}
+	return 0;
+}
+
 //Write the function printO() here
 
 int main(){
